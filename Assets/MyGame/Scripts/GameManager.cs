6df346392a91abf706b1855dd805e2e30ff3d550
ansohag <anna.hagenstein@htl-salzburg.ac.at>
2019-12-17 16:16:29 +0100

    Refactoring: deleted debuglogs in GameManager - console is now empty when game played

--- Assets/MyGame/Scripts/GameManager.cs
+++ Assets/MyGame/Scripts/GameManager.cs
@@ -15,24 +15,11 @@ public class GameManager : MonoBehaviour
 
     private void Awake()
     {
-        if(instance == null)
+        if (instance == null)
         {
             instance = this;
         }
     }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log("in Start");   
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Debug.Log("in Update");
-    }
-
     public void GameOver()
     {
         ObstacleSpawner.instance.gameOver = true;
@@ -44,10 +31,9 @@ public class GameManager : MonoBehaviour
     {
         TextureScroll[] scrollingObjects = FindObjectsOfType<TextureScroll>();
 
-        foreach(TextureScroll item in scrollingObjects)
+        foreach (TextureScroll item in scrollingObjects)
         {
             item.scroll = false;
-            Debug.Log(item.name);
         }
     }
 
