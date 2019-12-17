8e029d7f385d58993d7af12de6eeddc43ce3c4fa
ansohag <anna.hagenstein@htl-salzburg.ac.at>
2019-12-17 14:27:23 +0100

    refactoring: deleted unnecessary space in TextureScroll Script

--- Assets/MyGame/Scripts/TextureScroll.cs
+++ Assets/MyGame/Scripts/TextureScroll.cs
@@ -4,16 +4,13 @@ using UnityEngine;
 
 public class TextureScroll : MonoBehaviour
 {
-
     public float scrollSpeed;
     public bool scroll = true;
     Material backgroundMaterial;
-
     private void Awake()
     {
         backgroundMaterial = GetComponent<Renderer>().material;
     }
-
     private void FixedUpdate()
     {
         if (scroll)
@@ -21,7 +18,6 @@ public class TextureScroll : MonoBehaviour
             Vector2 offset = new Vector2(scrollSpeed * Time.time, 0f);
 
             backgroundMaterial.mainTextureOffset = offset;
-
         }
     }
 }
