641b72448be4a16d7a5615a15d26576eb48f75c2
ansohag <anna.hagenstein@htl-salzburg.ac.at>
2019-12-17 14:03:44 +0100

    refactoring: deleted unnecessary spaces in MenuController Script

--- Assets/MyGame/Scripts/MenuController.cs
+++ Assets/MyGame/Scripts/MenuController.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MenuController : MonoBehaviour{
-    public void Play(){
+public class MenuController : MonoBehaviour
+{
+    public void Play()
+    {
         SceneManager.LoadScene("MainScene");
     }
-
     public void Exit()
     {
         Application.Quit();
--- Assets/MyGame/Scripts/Obstacle.cs
+++ Assets/MyGame/Scripts/Obstacle.cs
@@ -4,28 +4,28 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-Rigidbody2D rb;
-[SerializeField] private float MoveSpeed;
+    Rigidbody2D rb;
+    [SerializeField] private float MoveSpeed;
 
-private void Awake()
-{
-    rb = GetComponent<Rigidbody2D>();
-}
-
-// Start is called before the first frame update
-void Start()
-{
-        
-}
-
-// Update is called once per frame
-void Update()
-{
-    //if obstacle's position x is < -15f it will be destroyed
-    if(transform.position.x < -15f)
+    private void Awake()
     {
-        Destroy(gameObject);
-}
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if obstacle's position x is < -15f it will be destroyed
+        if (transform.position.x < -15f)
+        {
+            Destroy(gameObject);
+        }
         //if obstacle's position x is < -15f it will be destroyed
         if (transform.position.x > 15f)
         {
@@ -34,12 +34,8 @@ void Update()
 
     }
 
-
-
-private void FixedUpdate()
-{
-
-    rb.velocity = Vector2.left * MoveSpeed;
-
-}
+    private void FixedUpdate()
+    {
+        rb.velocity = Vector2.left * MoveSpeed;
+    }
 }
